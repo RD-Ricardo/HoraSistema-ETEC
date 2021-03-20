7776181d2de2587492c708fe91d90e315ed4bcb8
RD-Ricardo <ricardodias169@gmail.com>
2021-03-20 16:14:53 -0300

    Finalizando Programa

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HoraSistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------");
+            Console.WriteLine("--Hora do sistema--");
+            Console.WriteLine("-------------------");
+            DateTime now = DateTime.Now;
+            Console.WriteLine($"Neste momento são exatamente {now}.");
+            
+        }
+    }
+}
